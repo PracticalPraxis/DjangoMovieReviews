dcf8ac6393e6ff71c1e149ec27e214e18d0c4b25
PracticalPraxis <meadeconway@gmail.com>
2022-11-23 14:54:44 +0100

    Removed example secret key

--- moviereviews/settings.py
+++ moviereviews/settings.py
@@ -21,8 +21,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-77n^62tk--**hrtyhgm7#lcai3cz^(e=z@6=$6ug=@#t@63s61'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
